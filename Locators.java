5e06837f182e7f73b78fc1afc8ac36690ec2da9f
Shobha Rao <sraork@gmail.com>
2025-01-04 14:48:43 +0530

    Add files via upload
    updated 04/01/2025

--- Locators.java
+++ Locators.java
@@ -1,6 +1,10 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
 
 
 
@@ -8,9 +12,35 @@ public class Locators {
 	
 	public static void main(String[] args) {
 		
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("Shobha");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/locatorspractice");
+		
+		//implicit wait
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		WebElement username = driver.findElement(By.id("inputUsername"));
+		username.sendKeys("Shobha");
+		
+		WebElement password = driver.findElement(By.name("inputPassword"));
+		password.sendKeys("admin");
+		
+		WebElement signup = driver.findElement(By.className("signInBtn"));
+		signup.click();
+		
+	
+		//WebElement errormessage = driver.findElement(By.cssSelector("p.error"));
+		WebElement errormessage = driver.findElement(By.className("error"));
+		System.out.println(errormessage.getText());
+		
+       
+	   driver.quit();
+		
+		
+		
+		
+		
+		
 		
 		
 		
